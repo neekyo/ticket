e84563a75fe4078b86d1203035807638a5822d27
neekyo <codebyns@gmail.com>
2024-11-18 14:47:57 -0500

    middleware & route error handling set up

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -12,8 +14,19 @@ router.post(
 			.isLength({ min: 4, max: 20 })
 			.withMessage('Password must be between 4 and 20 characters'),
 	],
-	(req: Request, res: Response) => {
+	async (req: Request, res: Response) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			throw new RequestValidationError(errors.array());
+		}
+
 		const { email, password } = req.body;
+
+		console.log('Creating a user...');
+		throw new DatabaseConnectionError();
+
+		res.send({});
 	}
 );
 
