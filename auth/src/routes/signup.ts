846c576ab8e0d27b93f1e5eaf072cb150d809176
neekyo <codebyns@gmail.com>
2024-11-19 12:45:32 -0500

    signin/signout auth w/ jwt & cookie session set up

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 
@@ -15,25 +17,34 @@ router.post(
 			.isLength({ min: 4, max: 20 })
 			.withMessage('Password must be between 4 and 20 characters'),
 	],
+  validateRequest,
 	async (req: Request, res: Response) => {
-		const errors = validationResult(req);
-
-		if (!errors.isEmpty()) {
-			throw new RequestValidationError(errors.array());
-		}
-
 		const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+		const existingUser = await User.findOne({ email });
 
-    if (existingUser) {
-      throw new BadRequestError('Email in use');
-    }
+		if (existingUser) {
+			throw new BadRequestError('Email in use');
+		}
 
-    const user = User.build({ email, password });
-    await user.save();
+		const user = User.build({ email, password });
+		await user.save();
 
-    res.status(201).send(user);
+		// Generate JWT
+		const userJwt = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+			},
+			process.env.JWT_KEY! // ! for TS, we know it's defined
+		);
+
+		// Store it on session object
+		req.session = {
+			jwt: userJwt,
+		};
+
+		res.status(201).send(user);
 	}
 );
 
