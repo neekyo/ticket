a821695d3e6416fe078f089d01cce2dd7d383193
neekyo <codebyns@gmail.com>
2024-11-25 12:48:23 -0500

    ticket service done & updated tests, moving to order service now

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -6,6 +6,8 @@ declare global {
   var signin: () => string[];
 }
 
+jest.mock('../nats-wrapper');
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdagrsdf';
@@ -17,6 +19,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   if (mongoose.connection.db) {
     const collections = await mongoose.connection.db.collections();
 
